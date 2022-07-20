70fa5e3ec2ceb9a3a988dd550cf9466d4fefae6e
Habib Rehman <hrehma6@illinois.edu>
2022-07-20 16:49:28 +0200

    load_images.py now has parsed args

--- load_images.py
+++ load_images.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
 
 import txsim as tx
 import squidpy as sq
@@ -9,35 +8,56 @@ import skimage.io
 import skimage.measure
 import skimage.segmentation
 import numpy as np
+import argparse
 
 #INPUT: DAPI.tif, binary.tif
 #OUTPUT: label.mat, areas.csv
 #From config:
-image_file = "C:/Users/Habib/Projects/HMGU/tx_project/heart/raw_data/nuclei_PCW4.5_1_watershed.tif"
-binary = True #Note: assumes the binary image is segmented (e.g. watershed via ImageJ)
-segmentation_method = 'imagej'
-expand_nuclear_area = 10
+#image_file = "C:/Users/Habib/Projects/HMGU/tx_project/heart/raw_data/nuclei_PCW4.5_1_watershed.tif"
+#binary = True
+#segmentation_method = 'imagej'
+#expand_nuclear_area = 10
 
-#If unsegmented, segment image
-if(not binary):
-    img = sq.im.ImageContainer(image_file)
-    tx.preprocessing.segment_nuclei(img, layer = 'image', method=segmentation_method)
-    img_arr = img["segmented_{}".format(segmentation_method)].to_numpy()[:,:,0,0]
 
-#If already segmented, label
-else:
-    img_arr = skimage.io.imread(image_file)
-    img_arr = skimage.measure.label(img_arr, connectivity=1)
+if __name__ == '__main__':
 
-#Expand nuclear area to reflect whole cell area
-if(expand_nuclear_area != None):
-    img_arr = skimage.segmentation.expand_labels(img_arr, distance=expand_nuclear_area)
+    parser = argparse.ArgumentParser(description='Segment DAPI images')
+    parser.add_argument('-i', '--input', required=True, help='Input image file')
+    parser.add_argument('-o', '--output', required=True, help='Output directory')
+    parser.add_argument('-b', '--binary', default=False, 
+        help='If the input image is a segmented, binary image (e.g. watershed via ImageJ)')
+    parser.add_argument('-s', '--segment', default='watershed', 
+        help='Segmentation method to be used')
+    parser.add_argument('-e', '--expand', default=0, 
+        help='Amount to expand each segment by- can be used to approximate cell boundary') 
+    
+    args = parser.parse_args()
 
-#Save as .mat file
-scipy.io.savemat("data/label_{}.mat".format(segmentation_method), {'label':img_arr})
+    image_file = args.input
+    output = args.output
+    binary = args.binary
+    segmentation_method = args.segment
+    expand_nuclear_area = args.expand
 
-#Calculate and save areas
-(unique, counts) = np.unique(img_arr, return_counts=True)
-areas = np.asarray((unique, counts)).T
-np.savetxt('data/areas_{}.csv'.format(segmentation_method), areas, delimiter=",")
+    #If unsegmented, segment image
+    if(not binary):
+        img = sq.im.ImageContainer(image_file)
+        tx.preprocessing.segment_nuclei(img, layer = 'image', method=segmentation_method)
+        img_arr = img[f"segmented_{segmentation_method}"].to_numpy()[:,:,0,0]
 
+    #If already segmented, label
+    else:
+        img_arr = skimage.io.imread(image_file)
+        img_arr = skimage.measure.label(img_arr, connectivity=1)
+
+    #Expand nuclear area to reflect whole cell area
+    if(expand_nuclear_area != None):
+        img_arr = skimage.segmentation.expand_labels(img_arr, distance=expand_nuclear_area)
+
+    #Save as .mat file
+    scipy.io.savemat(f"{output}/label_{segmentation_method}.mat", {'label':img_arr})
+
+    #Calculate and save areas
+    (unique, counts) = np.unique(img_arr, return_counts=True)
+    areas = np.asarray((unique, counts)).T
+    np.savetxt(f'{output}/areas_{segmentation_method}.csv', areas, delimiter=",")
