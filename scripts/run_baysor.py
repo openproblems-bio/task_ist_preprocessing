be52c8478e95211ebbedf430184585d25aef4e17
LouisK92 <louiskuemmerle@googlemail.com>
2022-11-06 14:30:22 +0100

    Adapt run baysor script

--- scripts/run_baysor.py
+++ scripts/run_baysor.py
@@ -24,13 +24,13 @@ if __name__ == '__main__':
         
         # [Data]
         # Name of the x column in the input data. Default: "x"
-        'x-column' : "x",
+        'x-column' : '"x"',
         # Name of the y column in the input data. Default: "y"
-        "y-column" : "y",
+        "y-column" : '"y"',
         # Name of the y column in the input data. Default: "z"
-        "z-column" : "z",
+        "z-column" : '"z"',
         # Name of gene column in the input data. Default: "gene"
-        "gene-column" : "gene",
+        "gene-column" : '"Gene"',
         # Minimal number of molecules per gene. Default: 1
         "min-molecules-per-gene" : 1,
         # Minimal number of molecules for a cell to be considered as real. It's an important parameter, as it's used to infer several other parameters. Default: 3
@@ -38,24 +38,28 @@ if __name__ == '__main__':
         # Scale parameter, which suggest approximate cell radius for the algorithm. This parameter is required.
         "scale" : 50,
         # Standard deviation of scale across cells. Can be either number, which means absolute value of the std, or string ended with "%" to set it relative to scale. Default: "25%"
-        "scale-std" : "25%",
-        # Use scale estimate from DAPI if provided. Default: true
-        "estimate-scale-from-centers" : "true",
-        # Minimal number of molecules in a segmented region, required for this region to be considered as a possible cell. Default: min-molecules-per-cell / 4
-        "min-molecules-per-segment" : 2,
+        # "scale-std" : '"25%"',
         
-        #[Sampling] These parameters shouldn't normally be changed
-        # Prior weight of assignment a molecule to new component. Default: 0.2
-        "new-component-weight" : 0.2,
-        # Fraction of distributions, sampled at each stage. Default: 0.3
-        "new-component-fraction" : 0.3,
+        # # Use scale estimate from DAPI if provided. Default: true
+        # "estimate-scale-from-centers" : "true",
+        # # Minimal number of molecules in a segmented region, required for this region to be considered as a possible cell. Default: min-molecules-per-cell / 4
+        # # "min-molecules-per-segment" : 2, # TODO: throws Error ErrorException("Unexpected value in the config: 'min-molecules-per-segment'")
+        # 
+        # #[Sampling] These parameters shouldn't normally be changed
+        # # Prior weight of assignment a molecule to new component. Default: 0.2
+        # "new-component-weight" : 0.2,
+        # # Fraction of distributions, sampled at each stage. Default: 0.3
+        # "new-component-fraction" : 0.3,
         
         #[Plotting]
         # Number of neighbors (i.e. 'k' in k-NN), which is used for gene composition visualization. Larger numbers leads to more global patterns. Default: estimate from min-molecules-per-cell
-        "gene-composition-neigborhood" : 20,
+        # "gene-composition-neigborhood" : 20, # TODO: ErrorException("Unexpected value in the config: 'gene-composition-neigborhood'")
         # Number of pixels per cell of minimal size, used to estimate size of the final plot. For most protocols values around 7-30 give enough visualization quality. Default: 15
-        "min-pixels-per-cell" : 15,
+        # "min-pixels-per-cell" : 15, # TODO: didn't check this one, plotting is not so important i guess
     }
+
+    
+    
     
     
     args = parser.parse_args()
@@ -64,31 +68,47 @@ if __name__ == '__main__':
     data = args.data
     segmentation_method = args.segment
     hyperparams = eval(args.hyperparams)
+    hparams = {}
     for key in DEFAULT_HYPERPARAMS:
-        if key not in hyperparams:
-            hyperparams[key] = DEFAULT_HYPERPARAMS[key]
+        if key in hyperparams:
+            hparams[key] = hyperparams[key]
+        else:
+            hparams[key] = DEFAULT_HYPERPARAMS[key]
     id_code = args.id_code
     segment = True if args.segment is not None else False
     temp = args.temp if args.temp is not None else data
     temp = Path(temp) / f"baysor_{id_code}"
+    toml_file = temp / 'config.toml'
 
     #temp.mkdir(parents=True, exist_ok=True)
     if not os.path.exists(temp):
         os.makedirs(temp)
 
-
-    baysor_cli = "run "
-    for key, val in hyperparams.items():
-        baysor_cli += f"--{key} {val} "
+    # Write toml
+    with open(toml_file, "w") as file:
+        for key, val in hparams.items():
+            # TODO: extend the toml headers, check https://github.com/LouisK92/Baysor/blob/master/configs/example_config.toml
+            if key == "x-column":
+                file.write(f'[Data]\n')
+            elif key == "new-component-weight":
+                file.write(f'\n[Sampling]\n')
+            file.write(f'{key} = {val}\n')
+    
+    
+    baysor_cli = f"run -c {toml_file} -o {temp}/ {molecules}"
+    #baysor_cli = "run "
+    #for key, val in hyperparams.items():
+    #    baysor_cli += f"--{key} {val} "
         
     if segment:
         print("Running Baysor with prior segmentation")
-        baysor_cli += f"-o {temp}/ {molecules} {data}/segments_{segmentation_method}.tif"
+        baysor_cli += f" {data}/segments_{segmentation_method}.tif"
+        #baysor_cli += f"-o {temp}/ {molecules} {data}/segments_{segmentation_method}.tif"
         #baysor_cli += f"{molecules} -o {temp} --save-polygons=geojson -p {data}/segments_{segmentation_method}.tif"
             
     else:
         print("Running Baysor without prior segmentation")
-        baysor_cli += f"-o {temp}/ {molecules}"
+        #baysor_cli += f"-o {temp}/ {molecules}"
         #baysor_cli += f"{molecules} -o {temp} --save-polygons=geojson"
 
     os.system(f'''/Baysor/bin/baysor {baysor_cli}''')
