cf8e5f0f66e0cfa81354e99c70b88824ccc0f408
Robrecht Cannoodt <rcannood@gmail.com>
2024-08-27 13:30:00 +0200

    update script

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -5,5 +5,4 @@ set -e
 echo ">> Downloading resources"
 
 # the sync_resources script uses the test_resources S3 URI's in the _viash.yaml to download the resources.
-common/sync_resources/sync_resources \
-  --delete
+common/scripts/sync_resources --delete
