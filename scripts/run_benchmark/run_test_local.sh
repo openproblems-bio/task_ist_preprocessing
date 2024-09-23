d4e30fec1b8e72846692a41ae29fe54fa7dc51e1
Robrecht Cannoodt <rcannood@gmail.com>
2024-09-23 23:08:51 +0200

    initial benchmark implementation

--- scripts/run_benchmark/run_test_local.sh
+++ scripts/run_benchmark/run_test_local.sh
@@ -6,14 +6,6 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-# remove this when you have implemented the script
-echo "TODO: once the 'run_benchmark' workflow has been implemented, update this script to use it."
-echo "  Step 1: replace 'task_template' with the name of the task in the following command."
-echo "  Step 2: replace the rename keys parameters to fit your run_benchmark inputs"
-echo "  Step 3: replace the settings parameter to fit your run_benchmark outputs"
-echo "  Step 4: remove this message"
-exit 1
-
 set -e
 
 echo "Running benchmark on test data"
@@ -28,9 +20,8 @@ nextflow run . \
   -profile docker \
   -resume \
   -c common/nextflow_helpers/labels_ci.config \
-  --id cxg_mouse_pancreas_atlas \
-  --input_train resources_test/task_template/cxg_mouse_pancreas_atlas/train.h5ad \
-  --input_test resources_test/task_template/cxg_mouse_pancreas_atlas/test.h5ad \
-  --input_solution resources_test/task_template/cxg_mouse_pancreas_atlas/solution.h5ad \
+  --id mouse_brain_combined \
+  --input_sc resources_test/task_ist_preprocessing/mouse_brain_combined/scrnaseq_reference.h5ad \
+  --input_sp resources_test/task_ist_preprocessing/mouse_brain_combined/raw_ist.zarr \
   --output_state state.yaml \
   --publish_dir "$publish_dir"
