ae390d92247240834eb02112139df5fb24188d5a
Robrecht Cannoodt <rcannood@gmail.com>
2024-10-01 14:26:05 +0200

    update script

--- scripts/run_benchmark/run_full_seqeracloud.sh
+++ scripts/run_benchmark/run_full_seqeracloud.sh
@@ -10,17 +10,31 @@ set -e
 
 # generate a unique id
 RUN_ID="run_$(date +%Y-%m-%d_%H-%M-%S)"
-publish_dir="s3://openproblems-data/resources/temp_ist_preprocessing/results/${RUN_ID}"
+publish_dir="s3://openproblems-data/resources/task_ist_preprocessing/results/${RUN_ID}"
+
+# input_dir="s3://openproblems-data/resources/task_ist_preprocessing/datasets"
+# cat > /tmp/params.yaml << HERE
+# param_list:
+
+#   - id: "mouse_brain_combined/rep1"
+#     input_sp: "$input_dir/mouse_brain_combined/rep1/output_sp.zarr"
+#     input_sc: "$input_dir/mouse_brain_combined/rep1/output_sc.h5ad"
+
+# output_sc: "\$id/output_sc.h5ad"
+# output_sp: "\$id/output_sp.zarr"
+# output_state: "\$id/state.yaml"
+# publish_dir: "$publish_dir"
+# HERE
 
 # write the parameters to file
 cat > /tmp/params.yaml << HERE
-input_states: s3://openproblems-data/resources/temp_ist_preprocessing/datasets/**/state.yaml
+input_states: s3://openproblems-data/resources/task_ist_preprocessing/datasets/**/state.yaml
 rename_keys: 'input_sc:output_sc;input_sp:output_sp'
 output_state: "state.yaml"
 publish_dir: "$publish_dir"
 HERE
 
-tw launch https://github.com/openproblems-bio/temp_ist_preprocessing.git \
+tw launch https://github.com/openproblems-bio/task_ist_preprocessing.git \
   --revision build/main \
   --pull-latest \
   --main-script target/nextflow/workflows/run_benchmark/main.nf \
@@ -29,10 +43,10 @@ tw launch https://github.com/openproblems-bio/temp_ist_preprocessing.git \
   --params-file /tmp/params.yaml \
   --entry-name auto \
   --config common/nextflow_helpers/labels_tw.config \
-  --labels temp_ist_preprocessing,full
+  --labels task_ist_preprocessing,full
 
 aws s3 sync \
-  s3://openproblems-data/resources/temp_ist_preprocessing/results \
-  resources/temp_ist_preprocessing/results \
+  s3://openproblems-data/resources/task_ist_preprocessing/results \
+  resources/task_ist_preprocessing/results \
   --profile op \
   --dryrun
