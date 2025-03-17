828bdc39cd09434f2100d9ca8b57dc26bc2a7f20
Habib Rehman <harehman@iu.edu>
2025-03-17 11:11:41 -0400

    Added parameters for resolvi

--- src/methods_expression_correction/resolvi_correction/script.py
+++ src/methods_expression_correction/resolvi_correction/script.py
@@ -13,6 +13,9 @@ par = {
   'input_spatial_with_cell_types': 'resources_test/task_ist_preprocessing/mouse_brain_combined/spatial_with_cell_types.h5ad',
   'celltype_key': 'cell_type',
   'output': '../resolvi_spatial_corrected.h5ad',
+  'n_hidden': 32,
+  'encode_covariates': False,
+  'downsample_counts': True
 }
 meta = {
   'name': 'gene_efficiency_correction',
@@ -26,9 +29,6 @@ print('Reading input files', flush=True)
 adata_sp = ad.read_h5ad(par['input_spatial_with_cell_types'])
 adata_sp.layers["normalized_uncorrected"] = adata_sp.layers["normalized"]
 
-
-#TODO add resolvi here
-
 print("Filter cells with <5 counts")
 sc.pp.filter_cells(adata_sp, min_genes=5)
 
@@ -40,13 +40,16 @@ print('Running ResolVI', flush=True)
 
 scvi.external.RESOLVI.setup_anndata(adata_sp, labels_key=par['celltype_key'], layer="counts")
 
-supervised_resolvi = scvi.external.RESOLVI(adata_sp, semisupervised=True)
-
+supervised_resolvi = scvi.external.RESOLVI(adata_sp, semisupervised=True, 
+  n_hidden = par['n_hidden'], 
+  encode_covariates = par['encode_covariates'], 
+  downsample_counts = par['downsample_counts'])
 supervised_resolvi.train(max_epochs=50)
 
 samples_corr = supervised_resolvi.sample_posterior(
         model=supervised_resolvi.module.model_corrected,
-        return_sites=['px_rate', 'obs'],
+        return_sites=['px_rate'],
+        summary_fun={"post_sample_q50": np.median},
         num_samples=20, return_samples=False, batch_size=4000) #batch_steps was not a parameter
 samples_corr = pd.DataFrame(samples_corr).T
 
@@ -66,8 +69,8 @@ adata_sp.obsm["X_resolVI"] = supervised_resolvi.get_latent_representation()
 # adata_sp.layers["generated_expression"] = scipy.sparse.csr_matrix(samples_corr.loc['post_sample_q25', 'obs'])
 # adata_sp.layers["generated_expression_mean"] = scipy.sparse.csr_matrix(samples_corr.loc['post_sample_means', 'obs'])
 
-adata_sp.layers["corrected_counts"] = adata_sp.layers['counts'].multiply((samples_corr.loc['post_sample_q05', 'px_rate'] / (
-    1.0 + samples_corr.loc['post_sample_q05', 'px_rate'] + samples.loc['post_sample_means', 'mean_poisson']))).tocsr()
+adata_sp.layers["corrected_counts"] = adata_sp.layers['counts'].multiply((samples_corr.loc['post_sample_q50', 'px_rate'] / (
+    1.0 + samples_corr.loc['post_sample_q50', 'px_rate'] + samples.loc['post_sample_means', 'mean_poisson']))).tocsr()
 
 # Write output
 print('Writing output', flush=True)
