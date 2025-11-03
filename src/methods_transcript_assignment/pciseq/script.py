5c64bd5ad6832936154fa2d7bdc2bb0758b821ba
LouisK92 <louiskuemmerle@googlemail.com>
2025-11-03 17:32:59 +0100

    filter points at label image border in pciseq

--- src/methods_transcript_assignment/pciseq/script.py
+++ src/methods_transcript_assignment/pciseq/script.py
@@ -121,6 +121,12 @@ if isinstance(sdata_segm["segmentation"], xr.DataTree):
 else:
      label_image = sdata_segm["segmentation"].to_numpy()
 
+# There might be spots at the border of the image, pciseq runs into an error if this is the case
+transcripts_at_border = transcripts_dataframe['x'] > (label_image.shape[1]-0.5)
+transcripts_dataframe = transcripts_dataframe.loc[transcripts_at_border]
+transcripts_at_border = transcripts_dataframe['y'] > (label_image.shape[0]-0.5)
+transcripts_dataframe = transcripts_dataframe.loc[transcripts_at_border]
+
 # Grab all the pciSeq parameters
 opts_keys = [#'exclude_genes',
   'max_iter',
