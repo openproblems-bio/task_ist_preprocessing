5641cbad343aeaee8603e0d3b067c1546d5b0962
LouisK92 <louiskuemmerle@googlemail.com>
2025-12-11 18:21:29 +0100

    Assume gene symbols in both adatas in mapmycells script

--- src/methods_cell_type_annotation/mapmycells/script.py
+++ src/methods_cell_type_annotation/mapmycells/script.py
@@ -7,9 +7,9 @@ from pathlib import Path
 ## VIASH START
 par = {
     'input_spatial_normalized_counts': 'resources_test/task_ist_preprocessing/mouse_brain_combined/spatial_normalized_counts.h5ad',
-    'input_scrnaseq_reference': 'resources_test/common/2023_yao_mouse_brain_scrnaseq_10xv2/dataset.h5ad',
-    'cell_type_column': 'cell_type',
-    "output": 'output.h5ad'
+    'input_scrnaseq_reference': 'resources_test/task_ist_preprocessing/mouse_brain_combined/scrnaseq_reference.h5ad',
+    'celltype_key': 'cell_type',
+    "output": 'spatial_with_celltypes.h5ad'
 }
 meta = { "temp_dir": './tmp/'}
 
@@ -18,40 +18,40 @@ meta = { "temp_dir": './tmp/'}
 TMP_DIR = Path(meta["temp_dir"] or "/tmp/")
 TMP_DIR.mkdir(parents=True, exist_ok=True)
 
-sdata = ad.read_h5ad(par['input_spatial_normalized_counts'])
-adata = ad.read_h5ad(par['input_scrnaseq_reference'])
+adata_sp = ad.read_h5ad(par['input_spatial_normalized_counts'])
+adata_sc = ad.read_h5ad(par['input_scrnaseq_reference'])
 
-if "counts" in adata.layers:
-    adata.X = adata.layers["counts"]
+if "counts" in adata_sc.layers:
+    adata_sc.X = adata_sc.layers["counts"]
 
-mapping = dict(zip(adata.var["feature_name"], adata.var["feature_id"]))
-sdata.var.index = sdata.var.index.map(mapping)
-adata.var_names_make_unique()  
-sdata.var.index = sdata.var.index.astype(str)
-sdata.var_names_make_unique()
-common_genes = list(set(sdata.var.index).intersection(adata.var.index))
+adata_sp.var_names = adata_sp.var_names.astype(str)
+adata_sc.var_names = adata_sc.var_names.astype(str)
+adata_sp.var_names_make_unique()
+adata_sc.var_names_make_unique()
 
-adata = adata[:, common_genes]
+common_genes = list(set(adata_sp.var.index).intersection(adata_sc.var.index))
+
+adata_sc = adata_sc[:, common_genes]
 sc_path = os.path.join(meta["temp_dir"],"sc_adata_processed.h5ad")
-adata.write_h5ad(sc_path)
+adata_sc.write_h5ad(sc_path)
 sp_path = os.path.join(meta["temp_dir"],"sp_processed.h5ad")
-sdata[:, common_genes].write_h5ad(sp_path)
+adata_sp[:, common_genes].write_h5ad(sp_path)
 
 
 
 precomputed_path = os.path.join(meta["temp_dir"],"precomputed_stats.h5ad")
 
 command = [
-        "python",
-        "-m",
-        "cell_type_mapper.cli.precompute_stats_scrattch",
-        "--h5ad_path",
-        sc_path,  
-        "--hierarchy",
-        "['cell_type']",
-        "--output_path",
-       precomputed_path
-    ]
+    "python",
+    "-m",
+    "cell_type_mapper.cli.precompute_stats_scrattch",
+    "--h5ad_path",
+    sc_path,  
+    "--hierarchy",
+    "['cell_type']",
+    "--output_path",
+   precomputed_path
+]
 
 subprocess.run(command)
 
@@ -59,38 +59,38 @@ data = {"None": common_genes}
 genes_file_path = os.path.join(meta["temp_dir"],"genes.json")
 with open(genes_file_path, "w") as json_file:
         json.dump(data, json_file, indent=2)
-command = [
-        "python",
-        "-m",
-        "cell_type_mapper.cli.from_specified_markers",
-        "--query_path",
-        sp_path,  
-        "--type_assignment.normalization",
-        "log2CPM", 
-        "--precomputed_stats.path",
-        precomputed_path,
-        "--query_markers.serialized_lookup",
-        genes_file_path,
-        "--csv_result_path",
-        os.path.join(meta["temp_dir"],"results.csv"),
-        "--extended_result_path",
-        os.path.join(meta["temp_dir"], "extended_results.json"),
-        "--flatten",
-        "True",
-        "--type_assignment.bootstrap_iteration", 
-        "1",
-        "--type_assignment.bootstrap_factor",
-        "1.0"
 
-    ]
+command = [
+    "python",
+    "-m",
+    "cell_type_mapper.cli.from_specified_markers",
+    "--query_path",
+    sp_path,  
+    "--type_assignment.normalization",
+    "log2CPM", 
+    "--precomputed_stats.path",
+    precomputed_path,
+    "--query_markers.serialized_lookup",
+    genes_file_path,
+    "--csv_result_path",
+    os.path.join(meta["temp_dir"],"results.csv"),
+    "--extended_result_path",
+    os.path.join(meta["temp_dir"], "extended_results.json"),
+    "--flatten",
+    "True",
+    "--type_assignment.bootstrap_iteration", 
+    "1",
+    "--type_assignment.bootstrap_factor",
+    "1.0"
+]
 
 subprocess.run(command)
 annotation_df = pd.read_csv(os.path.join(meta["temp_dir"],"results.csv"), skiprows=3)
-sdata.obs[par['cell_type_column']] = list(annotation_df['cell_type_label'])
+adata_sp.obs[par['celltype_key']] = list(annotation_df['cell_type_label'])
 
 
 
-# Loop through all files in the folder
+# Delete all temporary files
 for file_path in [
      sc_path,
      sp_path,
@@ -103,4 +103,4 @@ for file_path in [
             os.remove(file_path)
 
 
-sdata.write_h5ad(par['output'])
+adata_sp.write_h5ad(par['output'])
